0c1b2d46db948786c3e5cf2fe8c6aba14ff767b5
Prajwal Y <prajwal299@gmail.com>
2015-09-16 17:50:28 -0400

    Code cleanup

--- p1/kern/console_driver.c
+++ p1/kern/console_driver.c
@@ -15,15 +15,30 @@
 #define CONSOLE_SIZE (CONSOLE_WIDTH*CONSOLE_HEIGHT)
 #define CURSOR_INVISIBLE ((CONSOLE_WIDTH*CONSOLE_HEIGHT)+1)
 
-void set_hardware_cursor(int);
-void scroll_console();
-void set_char_at_pos(char, char, int);
-void get_char_at_pos(char *, char *, int);
+static void set_hardware_cursor(int);
+static void scroll_console();
+static void set_char_at_pos(char, char, int);
+//static void get_char_at_pos(char *, char *, int);
 
 int cur_pos = 0; //Keeps track of the current position of the cursor
 int cursor_visible = 1; //Boolean to keep track whether cursor is visible
 char cur_color = FGND_WHITE; //Keeps track of the current color
 
+
+/**
+ * @brief Puts a character in the current position of the cursor, and 
+ * increments the cursor position by 1. In case the current position of
+ * the cursor goes beyond the last possible valid value (depending on
+ * the size of the console), the console is scrolled by one row.
+ *
+ * Special characters such as '\n', '\r' and '\b' are handled separately.
+ *
+ * @param ch The character to be printed in the console at the current
+ * position
+ *
+ * @return the input character
+ *
+ */
 int putbyte(char ch) {
 
 	switch(ch) {
@@ -34,7 +49,7 @@ int putbyte(char ch) {
 			cur_pos -= cur_pos%CONSOLE_WIDTH;
 			break;
 		case '\b':
-			cur_pos--;
+			cur_pos?:cur_pos--;
 			set_char_at_pos(' ', cur_color, cur_pos);
 			break;
 		default:
@@ -56,6 +71,19 @@ int putbyte(char ch) {
 
 }
 
+/**
+ * @brief Prints a character string of a given length at the current
+ * position of the cursor. The position of the cursor is incremented
+ * by the length of the string to be printed. If the string is longer
+ * than the current line, the string is continued to be printed in the
+ * next line. If the string exceeds the available space in the console,
+ * the console is scrolled up by one row, and the string will be printed.
+ *
+ * @param s The string to be printed
+ * @param len Length of the string to be printed
+ *
+ * @return Void
+ */
 void putbytes(const char *s, int len) {
 	int i;
 	for(i = 0; i < len; i++) {
@@ -63,15 +91,44 @@ void putbytes(const char *s, int len) {
 	}
 }
 
+/**
+ * @brief Sets the foreground and background colors of the future
+ * characters to be printed in the console.
+ *
+ * @param color The new color that needs to be set
+ *
+ * @bug need to check if color is valid
+ *
+ * @return 0 is successful, -1 if the color code is invalid
+ */
 int set_term_color(int color) {
 	cur_color = (char)color;
 	return 0;
 }
 
+/**
+ * @brief Gets the current foreground and background color
+ * of the characters on the console
+ *
+ * @param color The address to which the current color
+ * will be written
+ *
+ * @return Void
+ */
 void get_term_color(int *color) {
 	*color = (int)cur_color;
 }
 
+/**
+ * @brief Sets the logical cursor to the position defined
+ * by row and column. Subsequent prints will be continued
+ * from the new position of the cursor.
+ *
+ * @param row The new row of the cursor
+ * @param col The new column of the cursor
+ *
+ * @return 0 if successful, -1 if the new position is invalid
+ */
 int set_cursor(int row, int col) {
 	int pos = row * CONSOLE_WIDTH + col;
 	if(pos > CONSOLE_SIZE) {
@@ -84,21 +141,51 @@ int set_cursor(int row, int col) {
 	return 0;
 }
 
+/**
+ * @brief Gets the current position of the logical
+ * cursor
+ *
+ * @param row The address where the row value needs to be set
+ * @param col The address where the column value needs to be set
+ *
+ * @return Void
+ */
 void get_cursor(int *row, int *col) {
 	*row = cur_pos/CONSOLE_WIDTH;
 	*col = cur_pos%CONSOLE_WIDTH;
 }
 
+/**
+ * @brief Hides the cursor. Cursor will not be shown
+ * until show_cursor() is called.
+ *
+ * @return Void
+ */
 void hide_cursor() {
 	set_hardware_cursor(CURSOR_INVISIBLE);
 	cursor_visible = 0;
 }
 
+/**
+ * @brief Shows the cursor. If cursor is already shown,
+ * this will have no effect.
+ *
+ * @return void
+ */
 void show_cursor() {
-	cursor_visible = 1;
+	if(cursor_visible) {
+		return;
+	}
+	cursor_visible=1;
 	set_hardware_cursor(cur_pos);
 }
 
+/**
+ * @brief Clears the entire console. The cursor is reset
+ * to the first row and first column
+ *
+ * @return Void
+ */
 void clear_console() {
 	int i, j;
 	cur_pos = 0;
@@ -110,11 +197,33 @@ void clear_console() {
 	cur_pos = 0;
 }
 
+/**
+ * @brief Prints a given character at given row and
+ * column using the given color.
+ *
+ * @param row Row at which character needs to be printed
+ * @param col Column at which character needs to be printed
+ * @param ch Character to be printed
+ * @param color Color of the character to be printed
+ *
+ * @bug If any argument is invalid, the function should
+ * do nothing
+ *
+ * @return Void
+ */
 void draw_char(int row, int col, int ch, int color) {
 	int pos = ((row*CONSOLE_WIDTH) + col);
 	set_char_at_pos(ch, color, pos);
 }
 
+/** 
+ * @brief Returns the character displayed at position (row, col).
+ *
+ * @param row Row of the character.
+ * @param col Column of the character.
+ *
+ * @return The character at (row, col).
+ */
 char get_char(int row, int col) {
 	int pos = ((row*CONSOLE_WIDTH) + col);
 	return *(char *)(CONSOLE_MEM_BASE + (2*pos));
@@ -122,9 +231,11 @@ char get_char(int row, int col) {
 
 /*
  * @brief Sets the hardware cursor to the specified position
+ *
  * @param position of the cursor
  * Eg: If cursor has to be places at (x, y), then the value
  * of position would be ((x*row)+y)
+ *
  * @return void
  */
 void set_hardware_cursor(int position) {
@@ -140,24 +251,19 @@ void set_hardware_cursor(int position) {
  *
  */
 void scroll_console() {
-	int i, j;
-    for(i = 0; i < CONSOLE_HEIGHT-1; i++) {
+	int i;
 
-		memcpy((void *)(CONSOLE_MEM_BASE+(i*2*CONSOLE_WIDTH)), (void *)(CONSOLE_MEM_BASE+((i+1)*2*CONSOLE_WIDTH)), 2*CONSOLE_WIDTH);
+	//Copy all rows but the first one into a temporary buffer
+	int temp = (2*((CONSOLE_WIDTH*CONSOLE_HEIGHT)-CONSOLE_WIDTH));
+	char temp_buf[temp];
+	memcpy(temp_buf, (void *)(CONSOLE_MEM_BASE+(2*CONSOLE_WIDTH)), temp);
 
-        /*for(j = 0; j < CONSOLE_WIDTH; j++) {
-			int new_pos = ((i+1)*CONSOLE_WIDTH + j);
-			char ch, color;
-			get_char_at_pos(&ch, &color, new_pos);
-			
-			int pos = ((i*CONSOLE_WIDTH) + j);
-			set_char_at_pos(ch, color, pos);
-        }*/
-    }
+	//Copy the temporary buffer back to the start of the console
+	memcpy((void *)CONSOLE_MEM_BASE, temp_buf, temp);
 
 	//Set the last row to empty
-	for(j = 0; j < CONSOLE_WIDTH; j++) {
-		int pos = ((CONSOLE_HEIGHT-1)*CONSOLE_WIDTH)+j;
+	for(i = 0; i < CONSOLE_WIDTH; i++) {
+		int pos = ((CONSOLE_HEIGHT-1)*CONSOLE_WIDTH)+i;
 		set_char_at_pos(' ', cur_color, pos);
 	}
 
@@ -182,7 +288,7 @@ void set_char_at_pos(char ch, char color, int pos) {
  * @brief Gets the character and the color at the specified
  * position
  */
-void get_char_at_pos(char *ch, char *color, int pos) {
+/*void get_char_at_pos(char *ch, char *color, int pos) {
 	*ch = *(char *)(CONSOLE_MEM_BASE + (2*pos));
 	*color = *(char *)(CONSOLE_MEM_BASE + (2*pos+1));
-}
+}*/
--- p1/kern/install_driver.c
+++ p1/kern/install_driver.c
@@ -10,6 +10,7 @@
 
 #include <p1kern.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <simics.h>
 #include <asm.h>
 #include <keyhelp.h>
@@ -21,57 +22,109 @@
 #include "inc/keyboard_driver.h"
 #include "inc/timer_handler.h"
 
+/*IDT ENTRY CONSTANTS*/
+#define RESERVED 0
+#define SIZE 15
+#define DPL 0
+#define P 1
+
+/*TIMER CONSTANTS*/
+#define TIMER_PERIOD_LSB 100
+#define TIMER_PERIOD_MSB 0 
+
+/*OTHER CONSTANTS*/
+#define IDT_ENRTY_LENGTH 8
+
 /**
- * Data structure to pack the IDT entry
+ * Data structure to pack the IDT entry.
  */
 struct idt_entry {
 	unsigned int handler_offset_1:16;
 	unsigned int seg_selector:16;
 	unsigned int reserved:8;
 	unsigned int size:5;
-	unsigned int DPL:2;
-	unsigned int P:1;
+	unsigned int dpl:2;
+	unsigned int p:1;
 	unsigned int handler_offset_2:16;
-} keyboard_idt_entry, timer_idt_entry;
+};
 
+/*Helper functions*/
+static void populate_idt_entry(struct idt_entry *, void *);
+static void initialize_timer_frequency(void);
+static int add_idt_entry(int, void *);
+
+/**
+ * @brief Configures the keyboard and the timer handers
+ * by adding an entry in the IDT.
+ *
+ * @param tickback Function pointer of the callback function
+ * that needs to be called by the timer driver.
+ *
+ * @return 0 if interrupt handler is successful, -1 if not.
+ *
+ * @bugs Keyboard handler not installed
+ */
 int handler_install(void (*tickback)(unsigned int)) {
-	lprintf("Hello from handler_install function");
 
-	//Get base address for IDT
-	void *idt_base_addr = idt_base();
-	lprintf("idt_base: %p", idt_base_addr);
+	int retval;
 
 	//Create an IDT entry for trap gate for keyboard driver
-	/*void *idt_keyboard = ((char *)idt_base_addr)+(8*KEY_IDT_ENTRY);
-	lprintf("idt_keyboard: %p handler_address: %p", idt_keyboard, getchar);
-	keyboard_idt_entry.handler_offset_1 = (int)getchar;
-	keyboard_idt_entry.seg_selector = SEGSEL_KERNEL_CS;
-	keyboard_idt_entry.reserved = 0;
-	keyboard_idt_entry.size = 15;
-	keyboard_idt_entry.DPL = 0;
-	keyboard_idt_entry.P = 1;
-	keyboard_idt_entry.handler_offset_2 = ((int)getchar>>16);
-	memcpy(idt_keyboard, &keyboard_idt_entry, sizeof(keyboard_idt_entry));*/
 
 
 	//Initialize timer frequency
+	initialize_timer_frequency();
+
+	//Create an IDT entry for timer driver
+	retval = add_idt_entry(TIMER_IDT_ENTRY, timer_handler);
+
+	return retval;
+}
+
+/**
+ * @brief Initializes the timer frequency to generate interrupts
+ * every 10 milliseconds
+ * @return Void
+ */
+void initialize_timer_frequency() {
 	outb(TIMER_MODE_IO_PORT, TIMER_SQUARE_WAVE);
-	outb(TIMER_PERIOD_IO_PORT, 100);
-	outb(TIMER_PERIOD_IO_PORT, 0);
+	outb(TIMER_PERIOD_IO_PORT, TIMER_PERIOD_LSB);
+	outb(TIMER_PERIOD_IO_PORT, TIMER_PERIOD_MSB);
+}
 
-	void *timer_handler_asm = timer_handler;
+/**
+ * @brief Function to add an entry to the IDT.
+ *
+ * @param entry_index Index of the IDT entry to be created
+ * @param handler_addr Address of the handler
+ *
+ * @return 0 if successful, -1 if not.
+ */
+int add_idt_entry(int entry_index, void *handler_addr) {
+	void *idt_base_addr = idt_base();
+	void *idt_entry_addr = ((char *)idt_base_addr)+(IDT_ENRTY_LENGTH*entry_index);	
 
-	//Repeat the same for timer handler	
-	void *idt_timer = ((char *)idt_base_addr)+(8*TIMER_IDT_ENTRY);
-	lprintf("idt_timer: %p handler_address: %p", idt_timer, timer_handler_asm);
-	timer_idt_entry.handler_offset_1 = (int)timer_handler_asm;
-	timer_idt_entry.seg_selector = SEGSEL_KERNEL_CS;
-	timer_idt_entry.reserved = 0;
-	timer_idt_entry.size = 15;
-	timer_idt_entry.DPL = 0;
-	timer_idt_entry.P = 1;
-	timer_idt_entry.handler_offset_2 = ((int)timer_handler_asm>>16);
-	memcpy(idt_timer, &timer_idt_entry, sizeof(timer_idt_entry));	
+	struct idt_entry new_idt_entry;
+	populate_idt_entry(&new_idt_entry, handler_addr);
+	memcpy(idt_entry_addr, &new_idt_entry, sizeof(struct idt_entry));
 
-	return 0;
+	return 0; //TODO: FIX THIS
+}
+
+/**
+ * @brief Function to populate the idt_entry struct with the given
+ * interrupt handler
+ *
+ * @param idt_entry The entry which needs to be populated
+ * @param handler_addr Address of the interrupt handler
+ *
+ * @return Void
+ */
+void populate_idt_entry(struct idt_entry *idt_entry, void *handler_addr) {
+	idt_entry->handler_offset_1 = (int)handler_addr;
+	idt_entry->seg_selector = SEGSEL_KERNEL_CS;
+	idt_entry->reserved = RESERVED;
+	idt_entry->size = SIZE;
+	idt_entry->dpl = DPL;
+	idt_entry->p = P;
+	idt_entry->handler_offset_2 = ((int)handler_addr>>16);
 }
